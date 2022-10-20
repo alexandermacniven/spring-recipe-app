5d4178ee4a486e56e32c165ed4b3319f7355b7ea
Alex Macniven <alexander.macniven@landregistry.gov.uk>
2022-10-20 11:33:25 +0100

    Add Recipe View Page and Logic

---
+++ src/test/java/alex/springframework/springrecipeapp/controllers/RecipeControllerTest.java
@@ -0,0 +1,47 @@
+package alex.springframework.springrecipeapp.controllers;
+
+import alex.springframework.springrecipeapp.domain.Recipe;
+import alex.springframework.springrecipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+class RecipeControllerTest {
+
+    RecipeController recipeController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeController = new RecipeController(recipeService);
+    }
+
+    @Test
+    void getRecipeView() throws Exception{
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
--- src/test/java/alex/springframework/springrecipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/alex/springframework/springrecipeapp/services/RecipeServiceImplTest.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,5 +40,20 @@ class RecipeServiceImplTest {
         assertEquals(recipes.size(), 1);
 
         verify(recipeRepository, times(1)).findAll();
+    }
+
+    @Test
+    void getRecipeById() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipe1 = recipeService.findById(1L);
+
+        assertNotNull(recipe1);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
     }
 }
