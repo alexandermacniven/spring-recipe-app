d02d5a3a56c343db4e36e9bbf75b0db77d2c75cd
Alex Macniven <alexander.macniven@landregistry.gov.uk>
2022-10-20 13:51:35 +0100

    Add Command and Convert Objects

---
+++ src/main/java/alex/springframework/springrecipeapp/converters/NotesCommandToNotes.java
@@ -0,0 +1,26 @@
+package alex.springframework.springrecipeapp.converters;
+
+import alex.springframework.springrecipeapp.commands.NotesCommand;
+import alex.springframework.springrecipeapp.domain.Notes;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotesCommandToNotes implements Converter<NotesCommand, Notes> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Notes convert(NotesCommand source) {
+        if(source == null) {
+            return null;
+        }
+
+        final Notes notes = new Notes();
+        notes.setId(source.getId());
+        notes.setRecipeNotes(source.getRecipeNotes());
+        return notes;
+    }
+}
